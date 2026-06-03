47c70437455fb21fbd3293e6bf5d42f6e0d95464
xMintTea <minttea77105110116@gmail.com>
2026-06-03 13:20:06 +0800

    feat: add new server settings

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ import tailwindcss from '@tailwindcss/vite'
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  server: {
+    host: "127.0.0.1"
+  }
 })
 
 export default config
