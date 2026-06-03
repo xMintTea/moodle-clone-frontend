993de686afe23c76bfa02c2658517ee644dba2ed
xMintTea <minttea77105110116@gmail.com>
2026-06-03 13:20:30 +0800

    feat: update api.ts file

--- src/api.ts
+++ src/api.ts
@@ -1,42 +1,84 @@
 import axios from 'axios'
 
+const isServer = typeof window === 'undefined';
+const BASE_URL = "http://127.0.0.1:8000/v1/"
+let accessToken: string | null = null;
+let isRefreshing = false;
+let queue: ((token: string) => void)[] = [];
 
-const isBrowser = typeof window !== 'undefined'
 
-function getAccessToken(): string | null {
-  return isBrowser ? localStorage.getItem('access_token') : null
-}
-
-function getRefreshToken(): string | null {
-  return isBrowser ? localStorage.getItem('refresh_token') : null
-}
-
-function setTokens(access: string, refresh: string) {
-  if (isBrowser) {
-    localStorage.setItem('access_token', access)
-    localStorage.setItem('refresh_token', refresh)
-  }
-}
-
-function clearTokens() {
-  if (isBrowser) {
-    localStorage.removeItem('access_token')
-    localStorage.removeItem('refresh_token')
-  }
-}
-
+export const setAccessToken = (token: string | null) => {
+  accessToken = token;
+};
 
 
 const api = axios.create({
-    baseURL: "http://localhost:8000/v1/"
+    baseURL: BASE_URL,
+    withCredentials: true,
 })
 
-api.interceptors.request.use(config => {
-  const token = getAccessToken()
+api.interceptors.request.use(async (config) => {
+  let token = accessToken
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`
   }
   return config
 })
+
+const refresh = async (): Promise<string> => {
+  const { data } = await axios.post(
+    `${BASE_URL}auth/refresh`,
+    {},
+    { withCredentials: true }
+  );
+  const newToken = data.access_token;
+  setAccessToken(newToken);
+  return newToken;
+};
+
+
+
+
+api.interceptors.response.use(
+  (res) => res,
+   async (error) => {
+      const originalRequest = error.config;
+
+      if (error.response?.status !== 401 || originalRequest._retry) {
+        return Promise.reject(error);
+      }
+
+      if (isRefreshing) {
+        return new Promise((resolve) => {
+          queue.push((newToken: string) => {
+            originalRequest.headers.Authorization = `Bearer ${newToken}`
+            resolve(api(originalRequest))
+          });
+        });
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true
+
+      try {
+        const newToken = await refresh()
+        queue.forEach((cb) => cb(newToken))
+        queue = [];
+
+        originalRequest.headers.Authorization = `Bearer ${newToken}`
+        return api(originalRequest)
+      } catch (refreshError) {
+        queue = [];
+        setAccessToken(null);
+
+
+        return Promise.reject(refreshError);
+      } finally {
+        isRefreshing = false;
+      }
+   }
+)
+
 
 export default api
